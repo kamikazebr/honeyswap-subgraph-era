1d671c75bb35e41ea73046a2b7ff84c36c1b98a7
Paolo Guerra <pg.paologuerra@gmail.com>
2022-10-07 20:53:32 +0200

    refactor: remove pair token price handler

--- src/mappings/core.ts
+++ src/mappings/core.ts
@@ -19,9 +19,7 @@ import {
   createLiquiditySnapshot,
   addDailyUniqueAddressInteraction,
   addWeeklyUniqueAddressInteraction,
-  addMonthlyUniqueAddressInteraction,
-  createPairTokenPrice,
-  PairTokenPriceTimeframe,
+  addMonthlyUniqueAddressInteraction
 } from './helpers'
 import { getBundle, getSwaprFactory } from './factory'
 
@@ -594,49 +592,6 @@ export function handleSwap(event: Swap): void {
 
   // update unique monthly swaps
   addMonthlyUniqueAddressInteraction(event, swap.to)
-}
-
-export function handlePairTokenPrice(block: ethereum.Block): void {
-  let pairContractAddress = dataSource.address()
-  let pair = Pair.load(pairContractAddress.toHexString())
-
-  if (!pair) {
-    return
-  }
-
-  const blockTimestampDate = new Date(block.timestamp.toI64() * 1000)
-  const blockTimestampMinutes = blockTimestampDate.getUTCMinutes()
-  const blockTimestampHours = blockTimestampDate.getUTCHours()
-
-  const id = [
-    pair.id,
-    blockTimestampDate.getUTCFullYear().toString(),
-    blockTimestampDate.getUTCMonth().toString(),
-    blockTimestampHours.toString(),
-    blockTimestampMinutes.toString()
-  ].join('-')
-
-  const fiveMinutes = 5
-  const fifteenMinutes = 15
-  const twelveHours = 12
-
-  createPairTokenPrice(id, block, pair, PairTokenPriceTimeframe.ONE_MINUTE)
-
-  if (blockTimestampMinutes % fiveMinutes === 0) {
-    createPairTokenPrice(id, block, pair, PairTokenPriceTimeframe.FIVE_MINUTES)
-  }
-
-  if (blockTimestampMinutes % fifteenMinutes === 0) {
-    createPairTokenPrice(id, block, pair, PairTokenPriceTimeframe.FIFTEEN_MINUTES)
-  }
-
-  if (blockTimestampMinutes === 0) {
-    createPairTokenPrice(id, block, pair, PairTokenPriceTimeframe.ONE_HOUR)
-  }
-
-  if (blockTimestampHours % twelveHours === 0 && blockTimestampMinutes === 0) {
-    createPairTokenPrice(id, block, pair, PairTokenPriceTimeframe.TWELVE_HOURS)
-  }
 }
 
 /**
